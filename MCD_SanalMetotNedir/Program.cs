cacfc78fd148a8b85853538cc84d3e99c394b9dd
Faik Turan <turanfaik@hotmail.com>
2021-12-03 16:15:43 +0300

    Add project files.

---
+++ MCD_SanalMetotNedir/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Sanal Metot Nedir
+             * Sanal Metot dediğimiz baseclass içerisinde bulunan bir metotun istenirse türeyen sınıf içerisinde nasıl yeniden şekillenebileceğini inceleyeceğiz.
+             * 
+             * Kalıtım konusunda tüm nesnelerin object nesnesinden kalıtıldığını ve bazı metotların object nesnesinden bize geldiğini söylemiştik.
+             * Sanal metot anlatımına object içerisinde bize standart olarak gelen toStrin metotunu inceleyerek başlayacağız.
+             * Sanal metotlar, X-men filminde bulunan mystique karakterine benzetebiliriz.
+             * Ortama göre ken şeklini değiştirebilen bir karakter olması buna bir örnek olabilir.
+             * 
+             */ 
+        }
+    }
+}
